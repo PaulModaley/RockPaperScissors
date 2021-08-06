fa5e185a268bd20e96e8653ced7996e2e2edc89d
PaulModaley <pmodaley@gmail.com>
2021-08-06 16:40:10 +0000

    add variables in JS

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+const computerChoice = document.getElementById('computer-choice')
+const playerChoice = document.getElementById('player-choice')
+const result = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
